050ca0ad28e45a7cffdb553706c2c38edebe3b08
RizwansAccount <rizsid16@gmail.com>
2025-02-06 14:20:27 +0500

    set layouts

--- src/routes/AppLayout.jsx
+++ src/routes/AppLayout.jsx
@@ -1,13 +1,48 @@
+// import React from 'react';
+// import Sidebar from '../components/sidebar/Sidebar';
+// import { Outlet } from 'react-router-dom';
+
+// const AppLayout = () => {
+//   return (
+//     <>
+//       <Sidebar/>
+//       <Outlet/>
+//     </>
+//   )
+// }
+
+// export default AppLayout
 import React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
 import Sidebar from '../components/sidebar/Sidebar';
-import Navbar from '../components/navbar/Navbar';
+import { Outlet } from 'react-router-dom';
+
+const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme }) => ({
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    marginTop: '64px',
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: `calc(${theme.spacing(7)} + 1px)`,
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: `calc(${theme.spacing(1)} + 1px)`,
+    },
+  }),
+);
 
 const AppLayout = () => {
   return (
-    <>
-      <Sidebar/>
-    </>
-  )
-}
+    <Box sx={{ display: 'flex' }}>
+      <Sidebar />
+      <Main>
+        <Outlet />
+      </Main>
+    </Box>
+  );
+};
 
-export default AppLayout
+export default AppLayout;
